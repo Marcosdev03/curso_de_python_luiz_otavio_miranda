917b86206420f73f04bd3c2a08e8dc76b587ff2e
Marcosdev03 <sdfreitas7@gmail.com>
2024-11-04 17:06:21 -0300

    Mensagem sobre o que foi alterado

---
+++ aulas/aula3[DocString].py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Marcos paulo')
+print(1, 'Marcos Paulo"')
+
+# Aspas duplas
+print("Marcos Paulo")
+print(2, "Marcos Paulo'")
+
+# Escape
+print("Marcos\"Paulo\"")
+
+# r
+print(r"Marcos \"Paulo\"")

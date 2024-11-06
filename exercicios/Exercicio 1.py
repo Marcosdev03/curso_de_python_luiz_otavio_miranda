4319615f0659f48db5177cf87d7e4de6099ea2e1
Marcosdev03 <sdfreitas7@gmail.com>
2024-11-06 17:15:09 -0300

    fazendo teste

---
+++ exercicios/Exercicio 1.py
@@ -0,0 +1,13 @@
+nome = 'Marcos Paulo'
+sobrenome = 'Alves de Freitas'
+idade = 29
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18 
+altura_metros = 1.75
+
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('idade:',idade)
+print('Ano do nascimento:',ano_nascimento)
+print('É maior de idade?',maior_de_idade)
+print('Altura:',altura_metros)
